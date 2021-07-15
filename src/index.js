0e13810c454c868c1d90a7e6521eeaff486eb658
Zak Pie <zakpie@protonmail.com>
2021-07-15 15:11:37 +0000

    update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
  */
 const fsp = require("fs/promises")
 const path = require("path")
-//const ethSignUtil = require("eth-sig-util")
-//const Web3 = require("web3")
-var ethUtil = require('ethereumjs-util');
+const ethers = require("ethers")
+const EthCrypto = require("eth-crypto")
+
 
 const tokenIdLogFile = path.join(__dirname,".token_id");
 
@@ -42,61 +42,30 @@ module.exports = class TokenIdGenerator {
     async getId(){
 
         //lets get the next id 
-        let nextId = (await this._getNextId()).toString();
+        let nextId = (await this._getNextId());
 
-        let pkBuffer = Buffer.from(this.privateKey, 'hex')
+        let nextIdBN = ethers.BigNumber.from(nextId.toString())
 
-        const nextIdHex = ethUtil.addHexPrefix(this.padWithZeroes(ethUtil.stripHexPrefix(ethUtil.intToHex(nextId)), 64));
-        const message = ethUtil.keccakFromHexString(nextIdHex);
+        const messageHash = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["uint256"],[ nextIdBN ]));
+                
+        const sig   = EthCrypto.sign(this.privateKey,  messageHash);
 
-        //const message = Buffer.from(nextId);
-        
-        const msgHash = ethUtil.hashPersonalMessage(message);
-        const sig     = ethUtil.ecsign(msgHash, pkBuffer);
+        const v   = parseInt(sig.slice(130, 132), 16);
 
-        
-        var pub = ethUtil.ecrecover(msgHash, sig.v, sig.r, sig.s);
-        
-        var recoveredAddress = '0x' + ethUtil.pubToAddress(pub).toString('hex')
-
-        let accounAddress = "0x"+ethUtil.privateToAddress(pkBuffer).toString("hex")
-
-        if(accounAddress != recoveredAddress){
-            throw new Error("Failed to verify signed data")
-        }
-
-        //console.log("recoveredAddress ===>>", recoveredAddress, "==", accounAddress)
+        const vBN = ethers.BigNumber.from(v);
 
         let parsedResult = {
-            v: sig.v,
-            r: ethUtil.bufferToHex(sig.r), 
-            s: ethUtil.bufferToHex(sig.s),
-            tokenId: Number(nextId),
-            signature: ethUtil.bufferToHex(this.concatSig(sig.v, sig.r, sig.s))
+            r: sig.slice(0, 66),
+            s: '0x' + sig.slice(66, 130),
+            v: vBN,
+            tokenId: nextId,
+            signature: sig
         };
-
 
         return parsedResult;
     } //end fun 
 
 
-    concatSig(v, r, s) {
-        const rSig = ethUtil.fromSigned(r);
-        const sSig = ethUtil.fromSigned(s);
-        const vSig = ethUtil.bufferToInt(v);
-        const rStr = this.padWithZeroes(ethUtil.toUnsigned(rSig).toString('hex'), 64);
-        const sStr = this.padWithZeroes(ethUtil.toUnsigned(sSig).toString('hex'), 64);
-        const vStr = ethUtil.stripHexPrefix(ethUtil.intToHex(vSig));
-        return ethUtil.addHexPrefix(rStr.concat(sStr, vStr)).toString('hex');
-    }
-
-    padWithZeroes(number, length) {
-        let myString = `${number}`;
-        while (myString.length < length) {
-          myString = `0${myString}`;
-        }
-        return myString;
-    }
 
 }
 
